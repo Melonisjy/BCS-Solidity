8378e534b4ccded4b981380eaae801c69e428941
Melonisjy <dhy2457@naver.com>
2023-05-15 23:04:43 +0900

    constructor, modifier

--- contracts-lecture/230515-2.sol
+++ contracts-lecture/230515-2.sol
@@ -73,7 +73,7 @@ contract CONSTRUCTOR5 {
     address payable owner;
 
     constructor() payable{
-        payable(this).transfer(msg.value); // 배고할 때 msg.value만큼 contract에게 바로 입금
+        payable(this).transfer(msg.value); // 배포할 때 msg.value만큼 contract에게 바로 입금
         owner = payable(msg.sender); // 배포하는 지갑주소가 바로 owner로 설정
     }
 

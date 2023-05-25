bdc83b91168ed0318b48ac5a7379c0f4881093c8
Melonisjy <dhy2457@naver.com>
2023-05-26 01:16:25 +0900

    31~35 basic quiz

---
+++ contracts-basic/week4.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.18;
+
+contract _31 {
+    function isAorB(string memory _name) public pure returns(bool) {
+        return (keccak256(abi.encodePacked(_name)) == keccak256(abi.encodePacked("Alice")) || keccak256(abi.encodePacked(_name)) == keccak256(abi.encodePacked("Bob")));
+    }
+}
+
+contract _32 {
+    uint[] arr;
+    function getArray(uint _n) public {
+        require (_n % 3 == 0 && _n % 10 != 0);
+        arr.push(_n);
+    }
+}
+
+contract _33 {
+    struct Member {
+        string name;
+        uint number;
+        address account;
+        uint birth;
+    }
+
+    mapping (string => Member) nameSearch;
+
+    function setMember(string memory _name, uint _number, uint _birth) public {
+        nameSearch[_name] = Member(_name, _number, msg.sender, _birth);
+    }
+    
+    function getMemberByName(string memory _name) public view returns(Member memory) {
+        return nameSearch[_name];
+    } 
+}
+
+contract _34 {
+    struct student {
+        string name;
+        uint number;
+        uint score;
+    }
+
+    student[] students;
+
+    function getAvg() public view returns(uint) {
+        uint total;
+        for (uint i=0; i<students.length; i++) {
+            total += students[i].score;
+        }
+        return total / students.length;
+    }
+}
+
+contract _35 {
+		uint[] numbers;
+
+		function pushNumbers(uint[] memory _numbers) public returns(uint[] memory) {
+			for(uint i=0; i<1+_numbers.length/2;i+=2) {
+				numbers.push(_numbers[i+1]);
+			}
+			return numbers;
+		}
+}

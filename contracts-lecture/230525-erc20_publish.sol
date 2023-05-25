4d5ac87c273dc6020c9cdb88a760dd3b3a31a552
Melonisjy <dhy2457@naver.com>
2023-05-26 00:51:21 +0900

    first commit

---
+++ contracts-lecture/230525-erc20_publish.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract LIONTOKEN is ERC20("LIKE LION", "LL") {
+    constructor(uint _totalSupply) {
+        _mint(msg.sender, _totalSupply);
+    }
+
+    function getBalance() public view returns(uint) {
+        return balanceOf(msg.sender);
+    }
+}

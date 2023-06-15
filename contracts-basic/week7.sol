7032639adf0868d6d4f83480117155d1764a9592
Melonisjy <dhy2457@naver.com>
2023-06-15 23:28:16 +0900

    61~65

--- contracts-basic/week7.sol
+++ contracts-basic/week7.sol
@@ -8,7 +8,7 @@ contract _61 {
     }
 }
 
-// 2개의 숫자를 더하는 함수, 곱하는 함수 a의 b승을 반환하는 함수를 구현하는데 3개의 함수 모두 2개의 input값이 10을 넘지 않아야 하는 조건을 최대한 효율적으로 구현하세요.
+// 2개의 숫자를 더하는 함수, 곱하는 함수 a의 b승을 반환하는 함수를 구현하는데 3개의 함수 모두 2개의 input값이 10을 넘지 않아야 하는 조건을 최대한 효율적으로 구현하세요.sss
 contract _62 {
     modifier lessThanTen(uint a, uint b) {
         require (a < 10 && b < 10);
@@ -38,4 +38,11 @@ contract _63 {
 // 지갑 주소를 넣으면 5개의 4bytes로 분할하여 반환해주는 함수를 구현하세요.
 contract _64 {
     
+}
+
+// 숫자 3개를 입력하면 그 제곱을 반환하는 함수를 구현하세요. 그 3개 중에서 가운데 출력값만 반환하는 함수를 구현하세요.
+contract _65 {
+    function A(uint a, uint b, uint c) public pure returns(uint, uint, uint) {
+        return (a**2, b**2, c**2); 
+    }
 }
